680c29c129a84f39ec9382feb4f5664e23bdc616
mateuszgrzesiukiewicz <ajdija@gmail.com>
2017-08-26 17:32:16 +0200

    2.3-finished

--- src/components/ControlPanel/index.js
+++ src/components/ControlPanel/index.js
@@ -1,33 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ee from '../../eventEmitter';
 import store from '../../store';
 import Button from '../Button';
 
-class ControlPanel extends Component {
-    showHistory() {
-        ee.emitEvent('toggle-history');
-    }
-
-    clearDisplay() {
-        store.newExpression = 0;
-    }
-
-    removeOneChar() {
-        const curExpression = String(store.curExpression);
-        const newExpWithRemovedChar = curExpression.toString().trim().substring(0, (curExpression.length - 1));
-
-        return store.newExpression = newExpWithRemovedChar === '' ? 0 : newExpWithRemovedChar;
-    }
-
-    render() {
-        return (
-            <section className="buttons--controls">
-                <Button buttonClass="control" text="&larr;" clickHandler={this.removeOneChar}/>
-                <Button buttonClass="control" text="c" clickHandler={this.clearDisplay}/>
-                <Button buttonClass="control" text="history" clickHandler={this.showHistory}/>
-            </section>
-        )
-    }
+export const showHistory = () => {
+    ee.emitEvent('toggle-history');
 }
+
+export const clearDisplay = () => {
+    store.newExpression = 0;
+}
+
+export const removeOneChar = () => {
+    const curExpression = String(store.curExpression);
+    const newExpWithRemovedChar = curExpression.toString().trim().substring(0, (curExpression.length - 1));
+
+    return store.newExpression = newExpWithRemovedChar === '' ? 0 : newExpWithRemovedChar;
+}
+
+export const ControlPanel = () => (
+  <section className="buttons--controls">
+      <Button buttonClass="control" text="&larr;" clickHandler={removeOneChar}/>
+      <Button buttonClass="control" text="c" clickHandler={clearDisplay}/>
+      <Button buttonClass="control" text="history" clickHandler={showHistory}/>
+  </section>
+);
 
 export default ControlPanel;
