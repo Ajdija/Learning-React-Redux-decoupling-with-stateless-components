e953456ec157c500e454d701f25e5a9a0806bf3b
mateuszgrzesiukiewicz <ajdija@gmail.com>
2017-08-26 17:32:56 +0200

    2.3-finished

--- src/components/Operators/index.js
+++ src/components/Operators/index.js
@@ -1,37 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 import store from '../../store';
 import Button from '../Button';
 
-class Operators extends Component {
-    opHandler(type) {
-        store.newExpression = `${store.curExpression} ${type} `;
-    }
+export const opHandler = (type) => {
+    store.newExpression = `${store.curExpression} ${type} `;
+}
 
-    calculateExpression() {
-        /* eslint-disable */
-        // This rule is important in production apps!
-        // Read more: https://eslint.org/docs/rules/no-eval
-        // To simplify the functionality in this course we use eval
-        const calcFunc = eval;
-        /* eslint-enable */
-        try {
-            store.newExpression = calcFunc(store.curExpression);
-        } catch (e) {
-            console.error("Error: Incorrect Expression of digits & operators :(")
-        }
-    }
-
-    render() {
-        return (
-            <section className="buttons--operators">
-                {["+", "-", "*", "/"]
-                    .map((op, i) => (
-                        <Button key={i} text={op} clickHandler={this.opHandler}/>)
-                    )}
-                <Button text="=" clickHandler={this.calculateExpression}/>
-            </section>
-        )
+export const calculateExpression = () => {
+    /* eslint-disable */
+    // This rule is important in production apps!
+    // Read more: https://eslint.org/docs/rules/no-eval
+    // To simplify the functionality in this course we use eval
+    const calcFunc = eval;
+    /* eslint-enable */
+    try {
+        store.newExpression = calcFunc(store.curExpression);
+    } catch (e) {
+        console.error("Error: Incorrect Expression of digits & operators :(")
     }
 }
+
+export const Operators = () => (
+    <section className="buttons--operators">
+        {["+", "-", "*", "/"]
+            .map((op, i) => (
+                <Button key={i} text={op} clickHandler={opHandler}/>)
+            )}
+        <Button text="=" clickHandler={calculateExpression}/>
+    </section>
+)
 
 export default Operators;
