1898993e6d7912b47d3af4340f57039db715df45
mateuszgrzesiukiewicz <ajdija@gmail.com>
2017-08-26 16:54:53 +0200

    2.2-finished

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,24 +1,12 @@
 import React, { Component } from 'react';
 
-class Button extends Component {
-    constructor(props) {
-        super(props);
-        this.onClickHandler = this.onClickHandler.bind(this);
-    }
-
-    onClickHandler() {
-        if (this.props.clickHandler) {
-            this.props.clickHandler.call(null, this.props.text);
-        }
-    }
-
-    render() {
-        return (
-            <button className={this.props.buttonClass} onClick={this.onClickHandler}>
-                <span className="title">{this.props.text}</span>
-            </button>
-        );
-    }
-}
+export const Button = ({ text, buttonClass, clickHandler }) => (
+    <button
+      className={buttonClass}
+      onClick={() => clickHandler ? clickHandler.call(null, text) : null}
+    >
+        <span className="title">{text}</span>
+    </button>
+);
 
 export default Button;
